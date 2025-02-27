63ae207d56c06feb944b0c4a752ff3d50beb31c1
jshherrera <jsherrerah1@academia.usbbog.edu.co>
2025-02-26 19:42:43 -0500

    Update Tienda1_test.py

--- Tienda1/Tienda1_test.py
+++ Tienda1/Tienda1_test.py
@@ -20,8 +20,8 @@ class TestObtenerProducto(unittest.TestCase):
     """Clase para probar la función obtener_producto."""
 
     def test_obtener_producto(self):
-        """Prueba la obtención de un objeto Producto."""
-        # Simula la entrada del usuario
+        """Prueba."""
+        # Simulacion la entrada del usuario
         import io
         import sys
         entrada = io.StringIO("Pan\n2000\n10")
